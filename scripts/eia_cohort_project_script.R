4eccb311a9c676e73ee39bed16b38bebc984cb2a
Cedric Elkouh <>
2021-02-20 01:44:24 -0500

    select columns

--- scripts/eia_cohort_project_script.R
+++ scripts/eia_cohort_project_script.R
@@ -47,10 +47,33 @@ loadRData <- function(fileName){
 df_icpsr_screeners <- loadRData(file.path(ddir, 'ICPSR_37202', 'DS0001', '37202-0001-Data.rda'))
 df_icpsr_followups <- loadRData(file.path(ddir, 'ICPSR_37202', 'DS0003', '37202-0003-Data.rda'))
 
+# ICPSR data cut down to columns of interest
+df_icpsr_selected_cols <- df_icpsr_followups %>%
+  select(c('CDIVISON', 'METRO', 'QS7', 'INCOME', 'REGIONA', 'IWYEAR', 'AA1', 'AA5A', 'AE15',
+           'AB1', 'AF1', 'AF9', 'AF10', 'AQ10_1', 'AS5'))
+
 # Connecting Outcome Measures in Entrepreneurship, Technology, and Science (COMETS) database
 # https://www.kauffman.org/entrepreneurship/research/comets/
 # Note: read_csv works with zipped files
-df_test <- read_csv(file.path(ddir, 'COMETS', 'All CSV', 'Patent CSV', 'patent_citations_v2.csv.zip'))
+df_comets_patents <- read_csv(file.path(ddir, 'COMETS', 'All CSV', 'Patent CSV', 'patents_v2.csv.zip')) %>%
+  select(c('patent_id', 'grant_date', 'patent_title'))
+df_comets_patent_cite_counts <- read_csv(file.path(ddir, 'COMETS', 'All CSV', 'Patent CSV', 'patent_cite_counts_v2.csv.zip'))
+df_comets_patent_us_classes <- read_csv(file.path(ddir, 'COMETS', 'All CSV', 'Patent CSV', 'patent_us_classes_v2.csv.zip')) %>%
+  select(c('patent_id', 'us_class'))
+# This failed for some reason: 
+# df_comets_patent_assignees <- read_csv(file.path(ddir, 'COMETS', 'All CSV', 'Patent CSV', 'patent_assignees_v2.csv.zip')) %>%
+#   select(c('patent_id', 'org_type'))
+df_comets_patent_zd_cats <- read_csv(file.path(ddir, 'COMETS', 'All CSV', 'Patent CSV', 'patent_zd_cats_v2.csv.zip')) %>%
+  rename(patent_id = patent) %>%
+  select(c('patent_id', 'zd'))
+
+# Note: amount is NA for all values (probably to anonymize the data)
+df_comets_grants <- read_csv(file.path(ddir, 'COMETS', 'All CSV', 'Grant CSV', 'grants_v2.csv.zip')) %>%
+  select(c('grant_num', 'grant_agency', 'start_date', 'end_date', 'amount'))
+df_comets_grantee_orgs <- read_csv(file.path(ddir, 'COMETS', 'All CSV', 'Grant CSV', 'grantee_orgs_v2.csv.zip')) %>%
+  select(c('grant_num', 'grant_agency', 'org_type'))
+df_comets_grant_zd_cats <- read_csv(file.path(ddir, 'COMETS', 'All CSV', 'Grant CSV', 'grant_zd_cats_v2.csv.zip')) %>%
+  select(c('grant_num', 'grant_agency', 'zd'))
 
 # Subsetting
 subset_df <- function(df, n) {
@@ -58,12 +81,46 @@ subset_df <- function(df, n) {
   df[sample(1:nrow(df), n, replace=FALSE),]
 }
 
-df_test_subset <- subset_df(df_test, 10000)
+df_comets_patents_subset <- subset_df(df_comets_patents, 10000)
+df_comets_patent_subset <- df_comets_patents_subset %>%
+  left_join(df_comets_patent_cite_counts, by = 'patent_id') %>%
+  left_join(df_comets_patent_us_classes, by = 'patent_id') %>%
+  left_join(df_comets_patent_zd_cats, by = 'patent_id')
 
-# Additional datasets
-# Include: https://www.nber.org/research/data/nber-ces-manufacturing-industry-database
-# Consider: https://wayback.archive-it.org/5902/20181003221605/https://www.nsf.gov/statistics/iris/
-# Consider: https://www.nsf.gov/statistics/srvyberd/#tabs-1
+df_comets_grants_subset <- subset_df(df_comets_grants, 10000)
+df_comets_grant_subset <- df_comets_grants_subset %>%
+  left_join(df_comets_grantee_orgs, by = 'grant_num') %>%
+  left_join(df_comets_grant_zd_cats, by = 'grant_num')
+
+# NBER Manufacturing 
+# https://www.nber.org/research/data/nber-ces-manufacturing-industry-database
+df_nber_sic5811 <- read_csv(file.path(ddir, 'NBER Manufacturing', 'sic5811.csv')) %>%
+  select(c('sic', 'year', 'emp', 'pay', 'prode', 'prodw', 'vadd', 
+           'invest', 'dtfp5'))
+
+# Here are the three datasets we will use
+# We will "unsubset" at the end to run our regressions on the whole dataset
+View(df_comets_patent_subset)
+View(df_comets_grant_subset)
+View(df_nber_sic5811)
+  
+##
+
+# From the COMETS dataset, it seems there isn’t enough substantial data to base a whole paper on, 
+# but we could generally explore which industries receive the most funding from grants for patents 
+# and use this information as one point in a larger claim.  For the NBER data, we could examine how 
+# growth rates are related to capital expenditures in different industries or discuss how worker wages 
+# are proportionally adjusted as a company grows. In the last dataset, there is a lot we could analyze. 
+# We could look at how different industries promote entrepreneurial action by looking at categorical 
+# questions about attitudes towards a particular market or just generally the percentage of entrants 
+# and how much they invest in the start-up. (Arjun)
+
+# For COMETS, we can see whether different organization types receive different amounts in grant 
+# funding / support for patents. For NBER, we can see whether additional capital is related to increased 
+# value-added, and whether worker wages are affected. For PSED, we can look at geographic location, 
+# market competition, and motives for startup companies. I would expect larger investments in cities. (Chris)
+
+##
 
 # What we can do with this data
 # characteristics of high-patent industries (what are the incentives)
@@ -75,10 +132,6 @@ df_test_subset <- subset_df(df_test, 10000)
 # does greater r&d/new entrepreneurship in an industry increase likelihood patent will be cited?
 # might need to do some background about patent citations -- this could be the lit review for proposal
 # example: https://www.law.northwestern.edu/research-faculty/clbe/events/innovation/documents/AbramsSampatDrugCites060917.pdf
-
 # as subsidies increase, innovation decreases 
 # (so potentially as those go up in an industry, which would only happen in more concentrated industries,
 # innovation/patents might fall OR patents per researcher)
-
-# For next week
-# look through codebooks and find variables you want to explore
