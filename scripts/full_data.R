f631b391dd373cb2301a4969fa29ee46c910f640
Cedric Elkouh <gelkouh@uchicago.edu>
2021-04-10 12:25:40 -0400

    analysis data construction

---
+++ scripts/full_data.R
@@ -0,0 +1,189 @@
+# This snippet of code is a little loop that makes my code work on your computer
+root <- getwd()
+while(basename(root) != "eia-oeconomica") { # this is the name of your project directory you want to use
+  root <- dirname(root)
+}
+
+# This line runs the script in your data.R file so that each person can have
+# their data in a different place because everyone's file structure will be 
+# a little different
+source(file.path(root, "data.R"))
+
+# This is the specific folder we want to access data from
+ddir <- file.path(ddir, 'eia cohort project')
+
+# Loading the packages we want
+if (!require(tidyverse)) install.packages('tidyverse')
+library(tidyverse)
+library(readxl)
+if (!require(scales)) install.packages('scales')
+library(scales)
+if (!require(reshape2)) install.packages('reshape2')
+library(reshape2)
+if (!require(cowplot)) install.packages('cowplot')
+library(cowplot)
+if (!require(plm)) install.packages('plm')
+library(plm) # masks lag from dplyr (so we need to specify dplyr::lag)
+if (!require(stargazer)) install.packages('stargazer')
+library(stargazer) 
+if(!require(rvest)) install.packages('rvest')
+library(rvest)
+
+# We will consider six states: 
+# California (CA), Texas (TX), Illinois (IL), Massachusetts (MA), New York (NY), Florida (FL)
+state_list <- c('CA', 'TX', 'IL', 'MA', 'NY', 'FL')
+state_code <- c('06', '12', '17', '25', '36', '48')
+
+# COMETS 
+# https://www.kauffman.org/entrepreneurship/research/comets/
+univ_institutional_info <- read_csv(file.path(ddir, 'spring1/univ_institutional_info_v2.csv')) 
+univ_system_patents <- read_csv(file.path(ddir, 'spring1/univ_system_patents_v2.csv'))
+univ_org_info <- read_csv(file.path(ddir, 'spring1/univ_org_info_v2.csv'))
+
+univ_info <- univ_institutional_info %>%
+  inner_join(univ_org_info, by = 'unitid') %>%
+  filter(stabbr %in% state_list)
+
+univ_patents <- univ_info %>%
+  inner_join(univ_system_patents, by = c('org_id', 'yr')) %>%
+  rename(GeoName = city, year = yr) %>%
+  mutate(year = as.factor(year),
+         State = ifelse(stabbr == 'TX', 'Texas', 
+                        ifelse(stabbr == 'CA', 'California',
+                               ifelse(stabbr == 'IL', 'Illinois',
+                                      ifelse(stabbr == 'MA', 'Massachusetts',
+                                             ifelse(stabbr == 'FL', 'Florida', 'New York')))))) %>%
+  group_by(instnm, GeoName, State, stabbr, year) %>%
+  summarize(num_patents = sum(num_patents)) %>%
+  group_by(GeoName, State, stabbr, year) %>%
+  summarize(patent_universities_count = n_distinct(instnm), num_patents = sum(num_patents))
+
+# USPTO
+# https://www.uspto.gov/web/offices/ac/ido/oeip/taf/univ/org_gr/all_univ_ag.htm
+
+web_address <- 'https://www.uspto.gov/web/offices/ac/ido/oeip/taf/univ/org_gr/all_univ_ag.htm'
+webpage_code <- read_html(web_address)
+
+webpage_text <- html_text(html_nodes(webpage_code, '.data , .header'))
+index <- 1
+# index <- 26
+# tail(webpage_text, -25))
+for (item in webpage_text) {
+  webpage_text[index] <- str_trim(gsub(item, pattern = "\n", replacement = ""), side = 'both')
+  index <- index + 1
+}
+
+uspto_patents <- data.frame(matrix(unlist(webpage_text), ncol = 25, byrow = TRUE))
+colnames(uspto_patents) <- as.character(uspto_patents[1,])
+uspto_patents <- uspto_patents[-1,] %>%
+  select(-c('Total', 'PRE_1992')) %>%
+  melt(id=c('Organizational Identifier', 'State')) %>%
+  rename(year = variable, patent_count = value, NAME = `Organizational Identifier`, STATE = State)
+
+# HIFLD
+# https://hifld-geoplatform.opendata.arcgis.com/datasets/colleges-and-universities?geometry=25.454%2C-16.798%2C-24.819%2C72.130&selectedAttribute=NAICS_CODE
+univ_locations <- read_csv(file.path(ddir, 'Colleges_and_Universities.csv'))
+univ_locations <- univ_locations %>%
+  select(c('NAME', 'CITY', 'STATE'))
+
+# Error introduced here: only merges on full university names (e.g., see Harvard)
+# Maybe do a fuzzy match? 
+# Or go through by hand and edit names?
+univ_uspto_patents <- uspto_patents %>%
+  inner_join(univ_locations, by = c('NAME', 'STATE')) %>%
+  filter(STATE %in% state_list) %>%
+  rename(GeoName = CITY) %>%
+  mutate(year = as.factor(year),
+         patent_count = as.numeric(patent_count),
+         State = ifelse(STATE == 'TX', 'Texas', 
+                        ifelse(STATE == 'CA', 'California',
+                               ifelse(STATE == 'IL', 'Illinois',
+                                      ifelse(STATE == 'MA', 'Massachusetts',
+                                             ifelse(STATE == 'FL', 'Florida', 'New York')))))) %>%
+  group_by(GeoName, State, STATE, year) %>%
+  summarize(patent_universities_count = n_distinct(NAME), patent_count = sum(patent_count)) %>%
+  mutate(patent_universities_count = ifelse(patent_count == 0, 0, patent_universities_count)) %>%
+  filter(patent_universities_count > 0)
+
+# BEA
+# https://apps.bea.gov/regional/downloadzip.cfm
+# Deleted last four rows of each of these files by hand in Excel (generally bad practice...)
+bea_ca <- read_csv(file.path(ddir, 'CAINC6N/CAINC6N_CA_2001_2019.csv')) %>%
+  mutate(State = 'California')
+bea_tx <- read_csv(file.path(ddir, 'CAINC6N/CAINC6N_TX_2001_2019.csv')) %>%
+  mutate(State = 'Texas')
+bea_il <- read_csv(file.path(ddir, 'CAINC6N/CAINC6N_IL_2001_2019.csv')) %>%
+  mutate(State = 'Illinois')
+bea_ma <- read_csv(file.path(ddir, 'CAINC6N/CAINC6N_MA_2001_2019.csv')) %>%
+  mutate(State = 'Massachusetts')
+bea_ny <- read_csv(file.path(ddir, 'CAINC6N/CAINC6N_NY_2001_2019.csv')) %>%
+  mutate(State = 'New York')
+bea_fl <- read_csv(file.path(ddir, 'CAINC6N/CAINC6N_FL_2001_2019.csv')) %>%
+  mutate(State = 'Florida')
+
+bea_full <- rbind(bea_ca, bea_tx, bea_il, bea_ny, bea_fl) %>%
+  filter(GeoFIPS != '"36000"' & GeoFIPS != '"06000"' & GeoFIPS != '"48000"' &
+           GeoFIPS != '"17000"' & GeoFIPS != '"25000"' & GeoFIPS != '"12000"') %>%
+  mutate(GeoName = toupper(GeoName)) %>%
+  select(-c('GeoFIPS', 'Region', 'TableName', 'LineCode')) %>%
+  melt(id=c('GeoName', 'IndustryClassification', 'Description', 'Unit', 'State')) %>%
+  rename(year = variable, compensation = value) %>%
+  separate(GeoName, c('GeoName', 'state_code'), ', ') %>%
+  select(-c('state_code'))
+
+# Merge datasets
+univ_full <- bea_full %>%
+  inner_join(univ_uspto_patents, by = c('GeoName', 'year', 'State')) %>%
+  select(!c('State')) %>% 
+  rename(state = STATE, city = GeoName, naics = IndustryClassification, naics_description = Description,
+         compensation_unit = Unit)
+
+univ_naics_simple <- univ_full %>%
+  filter(nchar(naics) == 3)
+
+kbi_list <- c(325, 333, 334, 335, 512, 513, 211, 324, 332, 486)
+# cross check and flag with KBI list
+naics_codes_kbi <- univ_naics_simple %>%
+  filter(naics != '...') %>%
+  select(c('naics', 'naics_description')) %>%
+  distinct() %>%
+  mutate(naics = as.numeric(naics)) %>%
+  arrange(naics) %>%
+  mutate(kbi = ifelse(naics %in% kbi_list, 1, 0)) %>%
+  select(-naics_description)
+
+`%not_in%` <- purrr::negate(`%in%`)
+univ_kbi <- univ_naics_simple %>%
+  filter(naics != '...') %>%
+  filter(compensation %not_in% c('(D)', '(NA)', '(NM)')) %>%
+  mutate(naics = as.numeric(naics), compensation = as.numeric(compensation)) %>%
+  left_join(naics_codes_kbi, by = 'naics') %>%
+  select(-c('naics_description', 'compensation_unit')) %>%
+  group_by(year, city, state) %>% 
+  mutate(total_comp = sum(compensation)) %>%
+  group_by(year, city, state, kbi) %>%
+  mutate(kbi_comp = sum(compensation)) %>%
+  filter(kbi == 1) %>%
+  ungroup() %>%
+  mutate(kbi_share = kbi_comp/total_comp) %>%
+  select(c('year', 'city', 'state', 'patent_universities_count', 'patent_count', 'kbi_comp', 'kbi_share')) %>%
+  distinct() %>%
+  filter(kbi_comp > 0) %>%
+  mutate(ln_kbi_comp = log(kbi_comp))
+
+univ_kbi_pd <- pdata.frame(x = univ_kbi, index = c('city', 'year'))
+
+lm_fe_1 <- plm(ln_kbi_comp ~ patent_count,
+               data = univ_kbi_pd, model = "within",effect = "twoways")
+
+lm_fe_2 <- plm(kbi_comp ~ patent_universities_count + patent_count,
+               data = univ_kbi_pd, model = "within",effect = "twoways")
+
+# compensation is in thousands of dollars
+table <- stargazer(lm_fe_1, lm_fe_2, 
+                     digits = 3, header = FALSE, type = 'text', model.numbers = FALSE)
+
+lm1 <- lm(ln_kbi_comp ~ patent_count + year, data = univ_kbi)
+summary(lm1)
+
+# figure out how to deal with patent counts in university systems/with universities with odd naming styles
